7335bbd76d918647b2038640fa3878e5caee1a97
me-solli <toxe.ds@googlemail.com>
2025-12-18 00:26:25 +0100

    Update items.js

--- items.js
+++ items.js
@@ -1,26 +1,26 @@
-let items = JSON.parse(localStorage.getItem("lootItems")) || [
-    {
-        name: "Trang-Ouls Schuppen",
-        category: "Set",
-        stars: 4,
-        roll: "—",
-        contact: "me_solli",
-        status: "Verfügbar"
-    },
-    {
-        name: "Der Erlöser",
-        category: "Unique",
-        stars: 3,
-        roll: "281% ED / 75 DMG",
-        contact: "me_solli",
-        status: "Verfügbar"
-    },
-    {
-        name: "Ali Baba",
-        category: "Unique",
-        stars: 3,
-        roll: "108% / 11 Dex",
-        contact: "me_solli",
-        status: "Verfügbar"
-    }
+const items = [
+  {
+    name: "Trang-Ouls Schuppen",
+    category: "Set",
+    stars: "4",
+    roll: "—",
+    contact: "me_solli",
+    status: "Verfügbar"
+  },
+  {
+    name: "Der Erlöser",
+    category: "Unique",
+    stars: "3",
+    roll: "281% ED / 75 DMG",
+    contact: "me_solli",
+    status: "Verfügbar"
+  },
+  {
+    name: "Ali Baba",
+    category: "Unique",
+    stars: "3",
+    roll: "108% / 11 Dex",
+    contact: "me_solli",
+    status: "Verfügbar"
+  },
 ];
