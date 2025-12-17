13b4833c4bc4f0a681d3683dabbff648fcac1ba8
me-solli <toxe.ds@googlemail.com>
2025-12-17 23:00:23 +0100

    add items.js

---
+++ items.js
@@ -0,0 +1,18 @@
+const items = [
+  {
+    name: "Trang-Ouls Schuppen",
+    category: "Set",
+    rating: 4,
+    roll: "—",
+    contact: "me_solli",
+    status: "Verfügbar"
+  },
+  {
+    name: "Ali Baba",
+    category: "Unique",
+    rating: 3,
+    roll: "MF Waffe",
+    contact: "me_solli",
+    status: "Verfügbar"
+  }
+];
