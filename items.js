d70efa3f0bd5df61b0be9f3be3099286220e4611
me-solli <toxe.ds@googlemail.com>
2025-12-18 08:48:16 +0100

    Update items.js

--- items.js
+++ items.js
@@ -1,98 +1,100 @@
 const items = [
   {
-    name: "undefined",
-    category: "Unique - Schild",
+    name: "Unbekannt",
+    category: "Unique – Schild",
     stars: "★★★★☆",
     roll: "+80% def",
     contact: "me_solli",
-    status: "undefined"
+    status: "Verfügbar"
   },
   {
-    name: "undefined",
-    category: "Unique - Handschuhe",
+    name: "Unbekannt",
+    category: "Unique – Handschuhe",
     stars: "★★★☆☆",
     roll: "+25% def",
     contact: "me_solli",
-    status: "undefined"
+    status: "Verfügbar"
   },
   {
-    name: "undefined",
-    category: "Unique - Armbrust",
+    name: "Unbekannt",
+    category: "Unique – Armbrust",
     stars: "★★★☆☆",
     roll: "+195% ed",
     contact: "me_solli",
-    status: "undefined"
+    status: "Verfügbar"
   },
   {
-    name: "undefined",
-    category: "Unique - Schild",
+    name: "Unbekannt",
+    category: "Unique – Schild",
     stars: "★★★★☆",
     roll: "+121% def",
     contact: "me_solli",
-    status: "undefined"
+    status: "Verfügbar"
   },
   {
-    name: "undefined",
-    category: "Unique - Rüstung",
+    name: "Unbekannt",
+    category: "Unique – Rüstung",
     stars: "★★★★★",
     roll: "+24 all res | +120% def | mdr 9",
     contact: "me_solli",
-    status: "undefined"
+    status: "Verfügbar"
   },
   {
-    name: "undefined",
-    category: "Unique - Helm",
+    name: "Unbekannt",
+    category: "Unique – Helm",
     stars: "★★★★☆",
     roll: "+163% def | +30 all res | +5 maek",
     contact: "me_solli",
-    status: "undefined"
+    status: "Verfügbar"
   },
   {
-    name: "undefined",
-    category: "Unique - Helm",
+    name: "Unbekannt",
+    category: "Unique – Helm",
     stars: "★★★★☆",
     roll: "+162% def | +30 all res | +5 maek",
     contact: "me_solli",
-    status: "undefined"
+    status: "Verfügbar"
   },
   {
-    name: "undefined",
+    name: "Unbekannt",
     category: "Set – Rüstung",
     stars: "★★★☆☆",
-    roll: "",
+    roll: "—",
     contact: "me_solli",
-    status: "undefined"
+    status: "Verfügbar"
   },
   {
-    name: "undefined",
+    name: "Unbekannt",
     category: "Unique – Wurfspeer",
     stars: "★★★★☆",
     roll: "+200% ed | 6% ll",
     contact: "me_solli",
-    status: "undefined"
+    status: "Verfügbar"
   },
   {
-    name: "undefined",
+    name: "Unbekannt",
     category: "Unique – Wurfspeer",
     stars: "★★★★☆",
     roll: "+196% ed | 7% ll",
     contact: "me_solli",
-    status: "undefined"
+    status: "Verfügbar"
   },
   {
-    name: "undefined",
+    name: "Unbekannt",
     category: "Unique – Gürtel",
     stars: "★★★☆☆",
     roll: "+172% def",
     contact: "me_solli",
-    status: "undefined"
+    status: "Verfügbar"
   },
   {
-    name: "undefined",
+    name: "Unbekannt",
     category: "Unique – Zweihandschwert",
     stars: "★★★☆☆",
     roll: "+155% ed",
     contact: "me_solli",
-    status: "undefined"
-  },
+    status: "Verfügbar"
+  }
 ];
+
+export default items;
