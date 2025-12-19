e8d0ae730de023ac61b4327e1d840a199a2c646f
me-solli <toxe.ds@googlemail.com>
2025-12-20 00:28:45 +0100

    Update items.js

--- items.js
+++ items.js
@@ -1,7 +1,7 @@
 const items = [
   {
     name: "Lidlose Wand",
-    category: "Unique – Schild",
+    category: "unique",
     type: "Schild",
     stars: "★★★★☆",
     roll: "+80% def",
@@ -10,7 +10,7 @@ const items = [
   },
   {
     name: "Magierfaust",
-    category: "Unique – Handschuhe",
+    category: "unique",
     type: "Handschuhe",
     stars: "★★★☆☆",
     roll: "+25% def",
@@ -19,16 +19,16 @@ const items = [
   },
   {
     name: "Buriza-Do Kyanon",
-    category: "Unique – Armbrust",
+    category: "unique",
     type: "Waffe",
     stars: "★★★☆☆",
-    roll: "+195% ed",
+    roll": "+195% ed",
     contact: "me_solli",
     status: "Verfügbar"
   },
   {
     name: "Haut des Vipernmagiers",
-    category: "Unique – Rüstung",
+    category: "unique",
     type: "Rüstung",
     stars: "★★★★★",
     roll: "+24 all res | +120% def | mdr 9",
@@ -37,7 +37,7 @@ const items = [
   },
   {
     name: "Jalals Mähne",
-    category: "Unique – Helm",
+    category: "unique",
     type: "Helm",
     stars: "★★★★☆",
     roll: "+163% def | +30 all res | +5 maek",
@@ -46,7 +46,7 @@ const items = [
   },
   {
     name: "Trang-Ouls Schuppen",
-    category: "Set – Rüstung",
+    category: "set",
     type: "Rüstung",
     stars: "★★★☆☆",
     roll: "—",
@@ -54,44 +54,17 @@ const items = [
     status: "Verfügbar"
   },
   {
-    name: "Titans Rache",
-    category: "Unique – Wurfspeer",
+    name: "Bartucs Halsabschneider",
+    category: "unique",
     type: "Waffe",
     stars: "★★★★☆",
-    roll: "+200% ed | 6% ll",
-    contact: "me_solli",
-    status: "Verfügbar"
-  },
-  {
-    name: "Donnergotts Gedeihen",
-    category: "Unique – Gürtel",
-    type: "Gürtel",
-    stars: "★★★☆☆",
-    roll: "+172% def",
-    contact: "me_solli",
-    status: "Verfügbar"
-  },
-  {
-    name: "Der Großvater",
-    category: "Unique – Zweihandschwert",
-    type: "Waffe",
-    stars: "★★★☆☆",
-    roll: "+155% ed",
-    contact: "me_solli",
-    status: "Verfügbar"
-  },
-  {
-    name: "Eingebrannter Riesenzauber",
-    category: "Charme – Riesenzauber",
-    type: "Riesenzauber",
-    stars: "★★★★☆",
-    roll: "+1 kampffertigkeiten (paladin)",
+    roll: "+189% ed | 30% fhr | 9% ll | +20 str | +20 dex",
     contact: "me_solli",
     status: "Verfügbar"
   },
   {
     name: "Der Himmelsriss",
-    category: "Charme – Riesenzauber",
+    category: "charm",
     type: "Riesenzauber",
     stars: "★★★★★",
     roll: "-90% lr",
