0e32c770d1f6e7a97ca0b6b9a2a28c8ddca26200
me-solli <toxe.ds@googlemail.com>
2025-12-19 23:49:41 +0100

    Update items.js

--- items.js
+++ items.js
@@ -1,195 +1,102 @@
 const items = [
   {
-    "name": "Lidlose Wand",
-    "category": "Unique – Schild",
-    "stars": "★★★★☆",
-    "roll": "+80% def",
-    "contact": "me_solli",
-    "status": "Verfügbar"
+    name: "Lidlose Wand",
+    category: "Unique – Schild",
+    type: "Schild",
+    stars: "★★★★☆",
+    roll: "+80% def",
+    contact: "me_solli",
+    status: "Verfügbar"
   },
   {
-    "name": "Magierfaust",
-    "category": "Unique – Handschuhe",
-    "stars": "★★★☆☆",
-    "roll": "+25% def",
-    "contact": "me_solli",
-    "status": "Verfügbar"
+    name: "Magierfaust",
+    category: "Unique – Handschuhe",
+    type: "Handschuhe",
+    stars: "★★★☆☆",
+    roll: "+25% def",
+    contact: "me_solli",
+    status: "Verfügbar"
   },
   {
-    "name": "Buriza-Do Kyanon",
-    "category": "Unique – Armbrust",
-    "stars": "★★★☆☆",
-    "roll": "+195% ed",
-    "contact": "me_solli",
-    "status": "Verfügbar"
+    name: "Buriza-Do Kyanon",
+    category: "Unique – Armbrust",
+    type: "Waffe",
+    stars: "★★★☆☆",
+    roll: "+195% ed",
+    contact: "me_solli",
+    status: "Verfügbar"
   },
   {
-    "name": "Lidlose Wand",
-    "category": "Unique – Schild",
-    "stars": "★★★★☆",
-    "roll": "+121% def",
-    "contact": "me_solli",
-    "status": "Verfügbar"
+    name: "Haut des Vipernmagiers",
+    category: "Unique – Rüstung",
+    type: "Rüstung",
+    stars: "★★★★★",
+    roll: "+24 all res | +120% def | mdr 9",
+    contact: "me_solli",
+    status: "Verfügbar"
   },
   {
-    "name": "Haut des Vipernmagiers",
-    "category": "Unique – Rüstung",
-    "stars": "★★★★★",
-    "roll": "+24 all res | +120% def | mdr 9",
-    "contact": "me_solli",
-    "status": "Verfügbar"
+    name: "Jalals Mähne",
+    category: "Unique – Helm",
+    type: "Helm",
+    stars: "★★★★☆",
+    roll: "+163% def | +30 all res | +5 maek",
+    contact: "me_solli",
+    status: "Verfügbar"
   },
   {
-    "name": "Jalals Mähne",
-    "category": "Unique – Helm",
-    "stars": "★★★★☆",
-    "roll": "+163% def | +30 all res | +5 maek",
-    "contact": "me_solli",
-    "status": "Verfügbar"
+    name: "Trang-Ouls Schuppen",
+    category: "Set – Rüstung",
+    type: "Rüstung",
+    stars: "★★★☆☆",
+    roll: "—",
+    contact: "me_solli",
+    status: "Verfügbar"
   },
   {
-    "name": "Jalals Mähne",
-    "category": "Unique – Helm",
-    "stars": "★★★★☆",
-    "roll": "+162% def | +30 all res | +5 maek",
-    "contact": "me_solli",
-    "status": "Verfügbar"
+    name: "Titans Rache",
+    category: "Unique – Wurfspeer",
+    type: "Waffe",
+    stars: "★★★★☆",
+    roll: "+200% ed | 6% ll",
+    contact: "me_solli",
+    status: "Verfügbar"
   },
   {
-    "name": "Trang-Ouls Schuppen",
-    "category": "Set – Rüstung",
-    "stars": "★★★☆☆",
-    "roll": "—",
-    "contact": "me_solli",
-    "status": "Verfügbar"
+    name: "Donnergotts Gedeihen",
+    category: "Unique – Gürtel",
+    type: "Gürtel",
+    stars: "★★★☆☆",
+    roll: "+172% def",
+    contact: "me_solli",
+    status: "Verfügbar"
   },
   {
-    "name": "Titans Rache",
-    "category": "Unique – Wurfspeer",
-    "stars": "★★★★☆",
-    "roll": "+200% ed | 6% ll",
-    "contact": "me_solli",
-    "status": "Verfügbar"
+    name: "Der Großvater",
+    category: "Unique – Zweihandschwert",
+    type: "Waffe",
+    stars: "★★★☆☆",
+    roll: "+155% ed",
+    contact: "me_solli",
+    status: "Verfügbar"
   },
   {
-    "name": "Titans Rache",
-    "category": "Unique – Wurfspeer",
-    "stars": "★★★★☆",
-    "roll": "+196% ed | 7% ll",
-    "contact": "me_solli",
-    "status": "Verfügbar"
+    name: "Eingebrannter Riesenzauber",
+    category: "Charme – Riesenzauber",
+    type: "Riesenzauber",
+    stars: "★★★★☆",
+    roll: "+1 kampffertigkeiten (paladin)",
+    contact: "me_solli",
+    status: "Verfügbar"
   },
   {
-    "name": "Donnergotts Gedeihen",
-    "category": "Unique – Gürtel",
-    "stars": "★★★☆☆",
-    "roll": "+172% def",
-    "contact": "me_solli",
-    "status": "Verfügbar"
-  },
-  {
-    "name": "Der Großvater",
-    "category": "Unique – Zweihandschwert",
-    "stars": "★★★☆☆",
-    "roll": "+155% ed",
-    "contact": "me_solli",
-    "status": "Verfügbar"
-  },
-  {
-    "name": "Klinge des Ali Baba",
-    "category": "Unique – Schwert",
-    "stars": "★★★★★",
-    "roll": "mf +86% | extragold +215% | 2os",
-    "contact": "me_solli",
-    "status": "Verfügbar"
-  },
-  {
-    "name": "Klinge des Ali Baba",
-    "category": "Unique – Schwert",
-    "stars": "★★★★★",
-    "roll": "mf +86% | extragold +215% | 2os",
-    "contact": "me_solli",
-    "status": "Verfügbar"
-  },
-  {
-    "name": "Klinge des Ali Baba",
-    "category": "Unique – Schwert",
-    "stars": "★★★★★",
-    "roll": "mf +86% | extragold +215% | 2os",
-    "contact": "me_solli",
-    "status": "Verfügbar"
-  },
-  {
-    "name": "Eingebrannter Riesenzauber",
-    "category": "Charme – Riesenzauber",
-    "stars": "★★★★☆",
-    "roll": "+1 kampffertigkeiten (paladin)",
-    "contact": "me_solli",
-    "status": "Verfügbar"
-  },
-  {
-    "name": "Der Erlöser",
-    "category": "Unique – Szepter",
-    "stars": "★★★★☆",
-    "roll": "+2 paladin skills | -33% target def",
-    "contact": "me_solli",
-    "status": "Verfügbar"
-  },
-  {
-    "name": "Schnitters Tribut",
-    "category": "Unique – Stangenwaffe",
-    "stars": "★★★★★",
-    "roll": "33% ctc decrepify | +199% ed | 11% ll",
-    "contact": "me_solli",
-    "status": "Verfügbar"
-  },
-  {
-    "name": "Adlerhorn",
-    "category": "Unique – Bogen",
-    "stars": "★★★★☆",
-    "roll": "+1 amazon skills | +200% ed | ignores target def",
-    "contact": "me_solli",
-    "status": "Verfügbar"
-  },
-  {
-    "name": "Die Fäulniskluft",
-    "category": "Charme – Riesenzauber",
-    "stars": "★★★★★",
-    "roll": "bricht giftimmunität | -72% gegner-giftres",
-    "contact": "me_solli",
-    "status": "Verfügbar"
-  },
-  {
-    "name": "Donnerschlag",
-    "category": "Unique – Wurfspeer",
-    "stars": "★★★★★",
-    "roll": "+4 jav skills | -15% enemy light res | 20% ctc lightning",
-    "contact": "me_solli",
-    "status": "Verfügbar"
-  },
-  {
-    "name": "Tal Rashas Horadrimwaffen",
-    "category": "Set – Helm",
-    "stars": "★★★★★",
-    "roll": "10% ll | 10% ml | +15 all res",
-    "contact": "me_solli",
-    "status": "Verfügbar"
-  },
-  {
-    "name": "Bartucs Halsabschneider",
-    "category": "Unique – Assassinen-Klaue",
-    "stars": "★★★★☆",
-    "roll": "+189% ed | 30% fhr | 9% ll | +20 str | +20 dex",
-    "contact": "me_solli",
-    "status": "Verfügbar"
-  },
-  {
-    "name": "Der Himmelsriss",
-    "category": "Charme – Riesenzauber",
-    "stars": "★★★★★",
-    "roll": "-90% lr",
-    "contact": "me_solli",
-    "status": "Verfügbar"
+    name: "Der Himmelsriss",
+    category: "Charme – Riesenzauber",
+    type: "Riesenzauber",
+    stars: "★★★★★",
+    roll: "-90% lr",
+    contact: "me_solli",
+    status: "Verfügbar"
   }
 ];
 
