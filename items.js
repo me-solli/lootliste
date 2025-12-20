61e237e324689e32823b99f98ab042c2f1ef3075
me-solli <toxe.ds@googlemail.com>
2025-12-20 01:14:13 +0100

    Update items.js

--- items.js
+++ items.js
@@ -2,52 +2,52 @@ const items = [
   {
     name: "Bartucs Halsabschneider",
     type: "Waffe",
-    category: "Unique",
+    category: "unique",
     stars: "★★★★☆",
-    roll: "+80% ed",
+    roll: "+80% ed, +20 str, +20 dex",
     status: "Verfügbar",
     contact: "me_solli",
-    image: "img/item-default.png"
+    image: "img/items/bartucs.png"
   },
   {
     name: "Der Himmelsriss",
     type: "Riesenzauber",
-    category: "Zauber",
+    category: "zauber",
     stars: "★★★★★",
-    roll: "-75% blitzresi",
+    roll: "-70% blitzresi",
     status: "Verfügbar",
     contact: "me_solli",
-    image: "img/item-default.png"
+    image: "img/items/himmelsriss.png"
   },
   {
-    name: "Tal Rashas Wächterauge",
-    type: "Helm",
-    category: "Set",
-    stars: "★★★★☆",
-    roll: "+100% def",
+    name: "Herold von Zakarum",
+    type: "Schild",
+    category: "unique",
+    stars: "★★★★★",
+    roll: "+190% def",
     status: "Reserviert",
     contact: "me_solli",
-    image: "img/item-default.png"
+    image: "img/items/hoz.png"
   },
   {
-    name: "Nagelring",
-    type: "Ring",
-    category: "Unique",
-    stars: "★★★☆☆",
-    roll: "+30% mf",
+    name: "Tal Rashas Lidloses Auge",
+    type: "Helm",
+    category: "set",
+    stars: "★★★★☆",
+    roll: "+120 mana",
     status: "Vergeben",
     contact: "me_solli",
-    image: "img/item-default.png"
+    image: "img/items/tal-helm.png"
   },
   {
-    name: "Ist",
-    type: "Sonstiges",
-    category: "Rune",
-    stars: "★★★★★",
+    name: "Ist Rune",
+    type: "Rune",
+    category: "rune",
+    stars: "★★★☆☆",
     roll: "—",
     status: "Verfügbar",
     contact: "me_solli",
-    image: "img/item-default.png"
+    image: "img/items/ist.png"
   }
 ];
 
