eb0633012090571b3d5345e93c31f5bdb27dcd77
me-solli <toxe.ds@googlemail.com>
2026-01-16 23:00:37 +0100

    Update items.mock.js

--- items.mock.js
+++ items.mock.js
@@ -1,1 +1,33 @@
-
+// items.mock.js
+export const items = [
+  {
+    id: 1,
+    name: "Beispiel Schwert",
+    type: "waffe",
+    sub: "Waffe · Schwert",
+    roll: "+200% ed · +20 str",
+    rating: 4,
+    donor: "Testspieler",
+    icon: "img/icons/waffe.png"
+  },
+  {
+    id: 2,
+    name: "Beispiel Helm",
+    type: "helm",
+    sub: "Helm · Unique",
+    roll: "+30 str · +15 allres",
+    rating: 3,
+    donor: "AnotherGuy",
+    icon: "img/icons/helm.png"
+  },
+  {
+    id: 3,
+    name: "Sunder Charm",
+    type: "charm",
+    sub: "Charm · Unique",
+    roll: "–70% Blitzres",
+    rating: 5,
+    donor: "DiabloFan",
+    icon: "img/icons/charm.png"
+  }
+];
