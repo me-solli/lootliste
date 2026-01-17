1e5956590e5ea028f2b5cef9494392fd557b80ed
me-solli <toxe.ds@googlemail.com>
2026-01-17 18:46:05 +0100

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+
+const app = express();
+const PORT = 3000;
+
+const DATA_PATH = path.resolve("data/items.json");
+
+// statische Dateien (Screenshots)
+app.use("/uploads", express.static("uploads"));
+
+// GET /items
+app.get("/items", (req, res) => {
+  const raw = fs.readFileSync(DATA_PATH, "utf-8");
+  const items = JSON.parse(raw);
+  res.json(items);
+});
+
+app.listen(PORT, () => {
+  console.log("Backend läuft auf http://localhost:" + PORT);
+});
